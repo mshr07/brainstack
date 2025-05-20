9d986d0645182c69b7960219634c62047b2a6abb
mshr07 <bunnymsh@gmail.com>
2025-05-20 15:33:25 +0530

    Removes Single_Responsibility class
    Deleted the Single_Responsibility class which had a static main method printing "Employee Name:", aligning with SOLID principles.

--- System Design/Low Level Design/SOLID/Single_Responsibility.java
+++
@@ -1,9 +0,0 @@
-package SOLID;
-import java.util.ArrayList;
-
-public class Single_Responsibility {
-    public static void main(String[] args) {
-
-        System.out.println("Employee Name: ");
-    }
-}
