e07bac1cfb086d8c94a4a996660c488aae7db1ab
mshr07 <bunnymsh@gmail.com>
2025-05-20 16:02:50 +0530

    Test

--- System Design/Low Level Design/SOLID/Single_Responsibility_Principle.java
+++ System Design/Low Level Design/SOLID/Single_Responsibility_Principle.java
@@ -1,12 +1,39 @@
 package SOLID;
+
 //This principle states that a class should only have one responsibility.
 //Furthermore, it should only have one reason to change.
+class Book {
 
+    public String name;
+    public String author;
+    public String text;
 
+    //constructor, getters and setters
+
+    // methods that directly relate to the book properties
+    public String replaceWordInText(String word, String replacementWord){
+        return text.replaceAll(word, replacementWord);
+    }
+
+    public boolean isWordInText(String word){
+        return text.contains(word);
+    }
+}
+class BookPrinter {
+    public void PrintBook(String s ){
+        System.out.println(s);
+    }
+}
 
 public class Single_Responsibility_Principle {
     public static void main(String[] args) {
+        Book b1=new Book();
+        Book b2=new Book();
 
-        System.out.println("Employee Name: ");
+        b1.text="VK is the best batsman in the world";
+        b2.text="Dhoni is the best white ball captain in the world";
+        BookPrinter bp=new BookPrinter();
+        bp.PrintBook(b1.text);
+        bp.PrintBook(b2.text);
     }
 }
