47b323f7d21990fd2d9c08b87aafef7511fe6098
mshr07 <bunnymsh@gmail.com>
2025-05-20 15:09:25 +0530

    Test

--- System Design/Low Level Design/SOLID/Single_Responsibility.java
+++ System Design/Low Level Design/SOLID/Single_Responsibility.java
@@ -1,10 +1,9 @@
 package SOLID;
 import java.util.ArrayList;
 
-public class Single_Responsibility {    
+public class Single_Responsibility {
     public static void main(String[] args) {
-        Employee employee = new Employee("John Doe", "Software Engineer");
-        employee.displayEmployeeDetails();
-        System.out.println("Employee Name: " + employee.getName());
+
+        System.out.println("Employee Name: ");
     }
 }
