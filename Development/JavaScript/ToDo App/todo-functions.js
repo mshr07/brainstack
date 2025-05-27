dcce6c1b59db6c4804393a96b865f22c1125a623
mshr07 <bunnymsh@gmail.com>
2025-05-27 15:10:46 +0530

    Draft Version

--- Development/JavaScript/ToDo App/todo-functions.js
+++
@@ -1,9 +0,0 @@
-const getTodos = function (todos) {
-  const todosJSON = localStorage.getItem("todos");
-
-  if (todosJSON !== null) {
-    return JSON.parse(todosJSON);
-  } else {
-    return [];
-  }
-};
